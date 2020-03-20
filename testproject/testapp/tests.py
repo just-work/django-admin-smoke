07d4994079cf7962bcbaf4f26fe08237552351ad
tumb1er <zimbler@gmail.com>
2020-03-20 10:14:13 +0300

    Tests for test case meta class

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -1,6 +1,6 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 
-from admin_smoke.tests import AdminTests, AdminBaseTestCase
+from admin_smoke.tests import AdminTests, AdminBaseTestCase, BaseTestCase
 from testproject.testapp import admin, models
 
 
@@ -30,3 +30,29 @@ class ProjectAdminTestCase(AdminTests, AdminBaseTestCase):
 
     def prepare_deletion(self):
         self.task.delete()
+
+
+class BaseTestCaseMetaTestCase(BaseTestCase):
+    """ Tests for base testcase metaclass."""
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.project = models.Project.objects.create(name='project')
+        cls.project2 = models.Project.objects.create(name='project2')
+        cls.forget_object(cls.project2)
+        cls.project2.delete()
+
+    def test_1_first(self):
+        """ Modify something in memory."""
+        self.project.name = 'modified'
+        self.project.save()
+
+        self.project2.name = 'modified2'
+
+    def test_2_second(self):
+        """ class attributes are reset correctly."""
+        # This test fill pass only if running whole test case
+        self.assertEqual(self.project.name, "project")
+        # forgotten object is in modified state
+        self.assertEqual(self.project2.name, "modified2")
